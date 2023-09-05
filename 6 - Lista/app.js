bc2f2484c1b3d722624f50093074a91662a6d301
Roger-Kk <rogerkolossoski@gmail.com>
2023-09-04 22:07:49 -0300

    Lista - metade

---
+++ 6 - Lista/app.js
@@ -0,0 +1,18 @@
+/* Selecionar itens */
+
+//Variáveis
+const alert = document.querySelector('.alert');
+const form = document.querySelector('.list-form');
+const list = document.getElementById('list');
+const submitBtn = document.querySelector('.submit-btn');
+const container = document.querySelector('.list-container');
+const listList = document.querySelector('.list-list');
+const clearBtn = document.querySelector('.clear-btn');
+
+//Opção de edição
+let editElement;
+let editFlag = false;
+let editID = ''; 
+
+//Event listeners
+form.addEventListener()
